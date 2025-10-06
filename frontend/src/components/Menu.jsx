a82c257d83b1305d69579b3be0141878be86e6c3
Kevin-C-Silva <Kevin.silva158@etec.sp.gov.br>
2025-10-06 17:14:58 -0300

    Desenvolvendo componente Menu

---
+++ frontend/src/components/Menu.jsx
@@ -0,0 +1,13 @@
+import MenuItem from './MenuItem.jsx'
+
+const Menu = ({ items }) => {
+    return (
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+            {items.map((item) => (
+                <MenuItem key={item.id} {...item} />
+            ))}
+        </div>
+    )
+}
+
+export default Menu
